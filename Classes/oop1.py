93c71687d8ea5563474984b696f515cbb7a0f9c9
yuliya <igordiye@caltech.edu>
2018-07-27 17:14:13 -0700

    classes

--- Classes/oop1.py
+++ Classes/oop1.py
@@ -3,12 +3,12 @@
 # 1. defining and using a Class
 import datetime # we will use this for date objects
 
-class Person:
+class Person:    #object
 
-    def __init__(self, name, surname, birthdate, address, telephone, email):
-        self.name = name
+    def __init__(self, name, surname, birthdate, address, telephone, email): # __method__ (object itself, other parameters)
+        self.name = name     # attribute = parameter (info is storred in object's attributes)
         self.surname = surname
-        self.birthdate = birthdate
+        self.birthdate = birthdate # birthdate is itself an object (the date class is defined in the datetime module)
 
         self.address = address
         self.telephone = telephone
@@ -30,7 +30,7 @@ person = Person(
     "No. 12 Short Street, Greenville",
     "555 456 0987",
     "jane.doe@example.com"
-)
+)     # person = instance of the Person class
 
 print(person.name)
 print(person.email)
